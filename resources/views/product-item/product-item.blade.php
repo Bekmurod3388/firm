e0c45c2b774e6489d30c08b76840596e07638970
umka17 <uxuxxr21@gmail.com>
2021-08-10 22:03:16 +0500

    "add class to products__svg and more"

--- resources/views/product-item/product-item.blade.php
+++ resources/views/product-item/product-item.blade.php
@@ -1,48 +1,49 @@
 <x-layout >
     @section('header')
-        <x-header/>
+    <x-header/>
     @endsection
-        <div class="container-min products-item-page">
-            <div class="products__wrapper">
-                <a class="link products-item-link svg__link-icon-before" href="{{url()->previous() }}">{{__('about.back')}}</a>
-                <div class="products__item products__item--only">
-                    <div class="products__item-top">
-                        <img src="/storage/products/{{$product->img}}">
-                        <h3 class="products__item-title">
-
-                            <span class="products__item-title--big">{{$product->{'head_'.app()->getLocale()} }}</span></h3>
-                    </div>
-                    <p class="products__item-text">{{$product->{'description_'.app()->getLocale()} }}</p>
-                    <div class="products__item-bottom">
-                        <button class="products__get btn">{{__('about.product.donate')}}</button>
-                    </div>
+    <div class="container-min products-item-page">
+        <div class="products__wrapper">
+            <a class="link products-item-link svg__link-icon-before" href="../products.blade.php">Назад</a>
+            <div class="products__item products__item--only">
+                <div class="products__item-top">
+                    <img class="products__svg--network_hardware products__svg " src="img/svg/network_hardware.svg" alt="network_hardware" width="82" height="82">
+                    <h3 class="products__item-title">
+                        <span class="products__item-title--small">Сетевое</span>
+                        <span class="products__item-title--big">Оборудование</span></h3>
                 </div>
-                <ul class="breadcrumbs-list">
-                    <li class="breadcrumbs-item breadcrumbs-item--active" ><a class="link ">1</a></li>
-                    <li class="breadcrumbs-item"><a class="link svg__next-icon-before"  href="product-item-2.html"><span class="breadcrumbs-number">2</span></a></li>
-                </ul>
+                <p class="products__item-text">Технологии за последние десятилетия претерпели стремительное
+                    развитие. Повышение эффективности труда и сокращение расходов – лишь часть преимуществ успешного
+                    внедрения современной сетевой инфраструктуры в бизнес. </p>
+                <div class="products__item-bottom">
+                    <button class="products__get btn">Запросить</button>
+                </div>
             </div>
+            <ul class="breadcrumbs-list">
+                <li class="breadcrumbs-item breadcrumbs-item--active" ><a class="link ">1</a></li>
+                <li class="breadcrumbs-item"><a class="link svg__next-icon-before"  href="product-item-2.html"><span class="breadcrumbs-number">2</span></a></li>
+            </ul>
         </div>
+    </div>
 
     @section('footer')
-        <x-footer/>
+    <x-footer/>
     @endsection
     <div>
         <div class="modal-back modal__disable">
             <div class="card__blur form modal ">
                 <div class="modal__title">
-                    <p class="form__title">{{__('about.product.consult')}}</p>
+                    <p class="form__title">Запрос на консультацию</p>
                     <button class="modal__close">x</button>
                 </div>
 
-                <form action="{{route('admin.messages.store')}}" method="POST">
-                    @csrf
+                <form action="https://example.com">
                     <ul class="form__list">
-                        <li class="form__item"><input type="text" class="form__input" name="name" placeholder="{{__('index.contact.name')}}"></li>
-                        <li class="form__item"><input type="tel" class="form__input" name="phone" placeholder="+998 (__) ___-__-__"></li>
-                        <li class="form__item"><input type="email" class="form__input" name="email" placeholder="Email"></li>
+                        <li class="form__item"><input type="text" class="form__input" placeholder="Ф.И.О"></li>
+                        <li class="form__item"><input type="tel" class="form__input" placeholder="+998 (__) ___-__-__" required></li>
+                        <li class="form__item"><input type="email" class="form__input" placeholder="Email"></li>
                     </ul>
-                    <input type="submit" class="form__submit" value="{{__('index.contact.send')}}">
+                    <input type="submit" class="form__submit" value="Отправить">
                 </form>
             </div>
 </x-layout>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     @section('header')
-        <x-header/>
+    <x-header/>
     @endsection
 
     <section class="products products-page">
@@ -8,29 +8,25 @@
             <div class="products__wrapper">
                 <ul class="filter-list">
                     @foreach($categories as $category)
-                        <li class="filter-item ">
-                            <button data-listToggle="solution" onclick="categoryFilter({{$category->id}})"
-                                    class="filter-btn">{{$category->name}}</button>
-                        </li>
+                    <li class="filter-item filter-item--active">
+                        <button data-listToggle="solution" class="filter-btn">{{$category->name}}</button>
+                    </li>
                     @endforeach
                 </ul>
                 <ul class="products__list" data-filteredlist data-list="solution">
-                    @foreach($category->products as $product)
-                        <li class="products__item">
-                            <div class="products__item-top">
-                                <img src="/storage/products/{{$product->img}}">
-                                <h3 class="products__item-title">
-                                    <span
-                                        class="products__item-title--big">{{$product->{'head_'.app()->getLocale()} }}</span>
-                                </h3>
-                            </div>
-                            <p class="products__item-text">{{substr($product->{'description_'.app()->getLocale()} ,0,150)}}</p>
-                            <div class="products__item-bottom">
-                                <a class="products__more btn"
-                                   href="{{route('products-item',[$product->id])}}">{{__('about.product.more')}}</a>
-                                <button class="products__get btn">{{__('about.product.donate')}}</button>
-                            </div>
-                        </li>
+                    @foreach($products as $product)
+                    <li class="products__item">
+                        <div class="products__item-top">
+                            <img src="/storage/products/{{$product->img}}" class="products__svg" width="82" height="82">
+                            <h3 class="products__item-title">
+                                <span class="products__item-title--big">{{$product->{'head_'.app()->getLocale()} }}</span></h3>
+                        </div>
+                        <p class="products__item-text">{{substr($product->{'description_'.app()->getLocale()} ,0,150)}}</p>
+                        <div class="products__item-bottom">
+                            <a class="products__more btn" href="{{route('products-item',[$product->id])}}">{{__('about.product.more')}}</a>
+                            <button class="products__get btn">{{__('about.product.donate')}}</button>
+                        </div>
+                    </li>
                     @endforeach
                 </ul>
 
@@ -39,7 +35,7 @@
     </section>
 
     @section('footer')
-        <x-footer/>
+    <x-footer/>
     @endsection
     <div>
         <div class="modal-back modal__disable">
@@ -51,32 +47,11 @@
 
                 <form action="https://example.com">
                     <ul class="form__list">
-                        <li class="form__item"><input type="text" class="form__input"
-                                                      placeholder="{{__('index.contact.name')}}"></li>
-                        <li class="form__item"><input type="tel" class="form__input" placeholder="+998 (__) ___-__-__">
-                        </li>
+                        <li class="form__item"><input type="text" class="form__input" placeholder="{{__('index.contact.name')}}"></li>
+                        <li class="form__item"><input type="tel" class="form__input" placeholder="+998 (__) ___-__-__" required></li>
                         <li class="form__item"><input type="email" class="form__input" placeholder="Email"></li>
                     </ul>
                     <input type="submit" class="form__submit" value="{{__('index.contact.send')}}">
                 </form>
             </div>
-            <script>
-                var products = toArray({{json_encode($p_array)}})
-                var filterList = document.querySelector(".filter-list"),
-                    filterItems = document.querySelectorAll(".filter-item"),
-                    lists = document.querySelectorAll("[data-filteredlist]");
-                filterList && filterList.addEventListener("click", (function (e) {
-
-                    /* if (e.target.dataset.listtoggle) {
-                         for (var t = e.target, r = 0; r < filterItems.length; r++) filterItems[r].classList.remove("filter-item--active");
-                         t.parentElement.classList.add("filter-item--active");
-                         for (var n = e.target.dataset.listtoggle, a = document.querySelector('[data-list="'.concat(n, '"]')), o = 0; o < lists.length; o++) lists[o].classList.add("list--hidden");
-                         a.classList.remove("list--hidden")
-                     }*/
-                }));
-
-                function categoryFilter(id) {
-                    console.log(products ,id)
-                }
-            </script>
 </x-layout>
