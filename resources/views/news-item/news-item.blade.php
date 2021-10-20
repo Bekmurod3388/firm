1b39fb5027145f5ba148a0f071867437116de8c1
umka17 <uxuxxr21@gmail.com>
2021-10-20 12:06:05 +0500

    i hope it is last change for this project

--- resources/views/news-item/news-item.blade.php
+++ resources/views/news-item/news-item.blade.php
@@ -7,7 +7,7 @@
         <section class="news-item">
             <div class="news-item__wrapper">
                 <h2 class="news-item__title">{{ $post->{'header_'.app()->getLocale()} }}</h2>
-                <img  class="news-item__photo" src="'/storage/posts/{{$post->img}}'" alt="Главная картина новости">
+                <img  class="news-item__photo" src="/storage/posts/{{$post->img}}" alt="Главная картина новости">
                 <div class=" news-item__card">
                     <h2 class="news-item__title-second">{{ $post->{'header2_'.app()->getLocale()} }}</h2>
                     <p class="card__text news-item__card-text--top  ">{{ $post->{'description_'.app()->getLocale()} }}</p>
